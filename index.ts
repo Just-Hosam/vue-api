5a7da272ad99a8c285f03327fb33f390868df678
Just-Hosam <hosam_firas@hotmail.com>
2025-01-29 12:33:31 -0700

    update app domain

--- index.ts
+++ index.ts
@@ -847,7 +847,7 @@ const cors = require("cors")
 
 const app = express()
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://vue-structure-kw1w.vercel.app"],
+  origin: ["http://localhost:5173", "https://vue-structure.vercel.app"],
   optionsSuccessStatus: 200,
 }
 
